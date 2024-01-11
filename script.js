2077ef86d08313c0e4511cf6d159ce0de65ed9d6
Asimic James Uka <jayasimic@gmail.com>
2024-01-11 20:57:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -172,9 +172,22 @@ const myFavoriteFootballTeam = {
 };
 
 Object.freeze(myFavoriteFootballTeam);
+const { sport, team, year, players } = myFavoriteFootballTeam;
+const { coachName } = myFavoriteFootballTeam.headCoach;
+
+typeOfSport.textContent = sport;
+teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
+
+const setPlayerCards = (arr = players) => {
 
 
+  playerCards.innerHTML += arr.map(
+    ({ name, position, number, isCaptain, nickname }) => {
+      `<div class="player-card"></div>`
+    }
+  );
 
-const { sport, team } = myFavoriteFootballTeam;
 
-
+};
